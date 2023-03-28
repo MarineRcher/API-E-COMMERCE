31577e19667374d7030f49af378d5ffb548e41be
MarineRcher <106527470+MarineRcher@users.noreply.github.com>
2023-03-28 17:26:13 +0200

    add mysql

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-import Express from 'express';
-const app = Express() 
+import express from 'express';
+const app = express() 
 const port = 3000
 app.set('view engine' , 'ejs')
 
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`app listening on port ${port}`)
 })
+
 
 
 // affiche index page
 app.get('/', (req, res) => {
     res.render('../pages/index')
-  })
+    }
+)
+
+app.use(express.static("../public"));
 
 
