e57c52f13837bcc69bda8acf8248c93d9a9ab023
MarineRcher <106527470+MarineRcher@users.noreply.github.com>
2023-03-28 15:53:16 +0200

    add docker

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-const express = require('express');
-const app = express();
-app.set('view engine', 'ejs');
+import Express from 'express';
+const app = Express() 
+const port = 3000
+app.set('view engine' , 'ejs')
 
-app.use(express.static("public"));
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
+
+// affiche index page
 app.get('/', (req, res) => {
-    res.render('hello');
-});
+    res.render('../pages/index')
+  })
 
-app.listen(3000, () => {
-    console.log('App listening on port 3000');
-});
+
